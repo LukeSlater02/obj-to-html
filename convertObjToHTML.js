3fe5f1af47e50513c53cfd52f7e5e60e951a14d7
LukeSlater02 <lukeslater101@gmail.com>
2022-02-17 13:47:54 -0600

    v1, need to figure out how to get function output to show up in HTML

---
+++ convertObjToHTML.js
@@ -0,0 +1,36 @@
+const applComputer = {
+	id: 1,
+	manufacturer: "Apple",
+	make: "Macbook",
+	model: "Pro",
+	specs: {
+		memory: 32,
+		hardDrive: "500 GB",
+		processor: 2.4
+	}
+}
+
+const winComputer = {
+	id: 1,
+	manufacturer: "Windows",
+	make: "Surface",
+	model: "Pro",
+	specs: {
+		memory: 8,
+		hardDrive: "1 TB",
+		processor: 4.4
+	}
+}
+
+const compHTML = (pcObject) => {
+    const compHTML = 
+    `<section id= ${pcObject.id}>
+	<h1>${pcObject.manufacturer} ${pcObject.make}</h1>
+	<div>Model: ${pcObject.model}</div>
+	<div>Memory: ${pcObject.specs.memory} GB</div>
+	<div>Hard drive space: ${pcObject.specs.hardDrive}</div>
+	<div>Processor speed: ${pcObject.specs.processor} Ghz</div>
+    </section>`
+    return console.log(compHTML);
+}
+compHTML(applComputer)
